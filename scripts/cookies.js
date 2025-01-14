a829f7b9ffe52d842541c1d0c1b8e5242654bd4d
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-01-14 11:43:12 +0300

    feat(SUT-42): Complete Cookies modal

--- scripts/cookies.js
+++ scripts/cookies.js
@@ -2,12 +2,14 @@ export default function startCookies() {
   const cookies = document.querySelector('.cookies');
   const cookiesButton = document.querySelector('.cookies__button');
 
+  // Open on page load with a delay
   setTimeout(() => {
     cookies.classList.add('open');
-  }, 3000);
+  }, 2000);
 
+  // Close on click the cookies button
   cookiesButton.addEventListener('click', () => {
     cookies.classList.remove('open');
-    setTimeout(() => cookies.close(), 500); // Wait for the animation to finish
+    setTimeout(() => cookies.close(), 500);
   });
 }
