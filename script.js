a840201569b2ce0346582376d549f83175aa178d
Riaz Mirzaianov <7o.mseJak@gmail.com>
2025-01-14 21:03:14 +0300

    refactor(SUT-1): Reorder scripts

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 import startPreloader from './scripts/preloader.js';
 import startHeroSlides from './scripts/hero.js';
-import startMenu from './scripts/menu.js';
 import startCookies from './scripts/cookies.js';
+import startMenu from './scripts/menu.js';
 
 startPreloader();
 startHeroSlides();
-startMenu();
 startCookies();
+startMenu();
